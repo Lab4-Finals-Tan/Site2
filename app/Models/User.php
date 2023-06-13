7357dc61f1471cda75753467c37ddaf6c05b4165
ChloeAngelie <tan.chloeangelie>
2023-06-13 13:01:19 +0800

    LAB4 SITE 2 FINALLY DONE

---
+++ app/Models/User.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App\Models;
+    use Illuminate\Database\Eloquent\Model;
+
+    class User extends Model{
+        protected $table = 'tbluser2';
+        protected $fillable = [
+            'username','password','gender','jobid'
+        ];
+
+        public $timestamps = false;
+        protected $primaryKey = 'userid';
+        protected $hidden = [
+            'password',
+        ];
+    }
